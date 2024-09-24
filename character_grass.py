f4059705e4fbebd7a81e6d726066d08596fadbb3
polarune(laptop) <jumpcross123@naver.com>
2024-09-24 12:34:04 +0900

    윗줄 이동 완성

--- character_grass.py
+++ character_grass.py
@@ -6,6 +6,13 @@ open_canvas()
 grass = load_image('grass.png')
 boy = load_image('character.png')
 
+def draw_boy(x,y):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    boy.draw_now(x,y)
+    delay(0.01)
+
+
 def run_circle():
     print('CIRCLE')
     r,cx,cy = 300, 800//2, 600//2
@@ -14,16 +21,17 @@ def run_circle():
         theta = math.radians(degree)
         x = r * math.cos(theta) + cx
         y = r * math.sin(theta) + cy
-    
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        boy.draw_now(x,y)
-        delay(0.01)
+
+        draw_boy(x,y)
         
     pass
 
 def run_top():
     print('TOP')
+    for x in range(0,800,10):
+        draw_boy(x,550)
+
+    
     pass
 
 def run_right():
