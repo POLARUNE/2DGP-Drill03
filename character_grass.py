5125b128d0f99da9e5717aeb9d6d64c8a6790a2a
polarune(laptop) <jumpcross123@naver.com>
2024-09-24 11:50:21 +0900

    뼈대 완성(print로 무한 루프 구조 확인)

--- character_grass.py
+++ character_grass.py
@@ -4,44 +4,19 @@ import math
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+boy = load_image('character.png')
 
-x = 0
-y = 90
+def run_circle():
+    print('CIRCLE')
+    pass
 
-def draw():
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(x,y)
+def run_rectangle():
+    print('RECTANGLE')
+    pass
 
-
-# while(True)
-
-for i in range(0,1):
-    while(x<=760):
-        draw()
-        x = x+20
-        delay(0.01)
-
-    while(y<=560):
-        draw()
-        y = y+20
-        delay(0.01)
-
-    while(x>=30):
-        draw()
-        x = x-20
-        delay(0.01)
-
-    while(y>90):
-        draw()
-        y = y-20
-        delay(0.01)
-
-    while(x<400):
-        draw()
-        x = x+20
-        delay(0.01)
-
-
+    
+while True:
+    run_rectangle()
+    run_circle()
+    
 close_canvas()
