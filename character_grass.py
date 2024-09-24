c92be06a2deaced04971b74665764fa80e66924e
polarune(laptop) <jumpcross123@naver.com>
2024-09-24 12:12:18 +0900

    원 운동 구현 완료

--- character_grass.py
+++ character_grass.py
@@ -8,13 +8,19 @@ boy = load_image('character.png')
 
 def run_circle():
     print('CIRCLE')
+    r,cx,cy = 300, 800//2, 600//2
+
+    for degree in range(0,360,3):
+        theta = math.radians(degree)
+        x = r * math.cos(theta) + cx
+        y = r * math.sin(theta) + cy
     
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    boy.draw_now(400,300)
-    delay(0.01)
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        boy.draw_now(x,y)
+        delay(0.01)
     
-    pass
+pass
 
 def run_rectangle():
     print('RECTANGLE')
